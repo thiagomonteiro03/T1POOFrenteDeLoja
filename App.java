1e6e449b06c8693321d0a57ddb692fbc4a0408c3
thiagomonteiro03 <thiagomonteiro.trab@outlook.com>
2021-04-18 12:46:49 -0300

    projeto finalizado

--- App.java
+++ App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String args[]) {
 
         //Validação de Produto
-        System.out.println("-----------------------------------------");
+        System.out.println("------------------Validação de Produto-----------------------");
         Produto iphone = new Produto(100, "iPhone 12", 8500);
         Produto android = new Produto(99, "Galaxy S20", 3000);
         System.out.println("Produto - " + iphone);
@@ -12,7 +12,7 @@ public class App {
 
 
         //Validação de CatalogoProdutos
-        System.out.println("-----------------------------------------");
+        System.out.println("-----------------Validação de CatalogoProdutos------------------------");
         CatalogoProdutos catalogo = new CatalogoProdutos();
         catalogo.cadastraProduto(iphone);
         catalogo.cadastraProduto(android);
@@ -20,8 +20,8 @@ public class App {
         System.out.println("Catalogo - getProduto de android: " + catalogo.getProduto(99));
         System.out.println();
         
-        //Validação de Estoque
-        System.out.println("-----------------------------------------");
+        //Validação de Estoque e ItemEstoque
+        System.out.println("-------------------Validação de Estoque e ItemEstoque----------------------");
         Estoque estoque = new Estoque();
         estoque.cadastraProduto(iphone, 10);
         estoque.cadastraProduto(android, 8);
@@ -33,25 +33,25 @@ public class App {
         System.out.println("Estoque - Pegando produto android: " + estoque.getProduto(99));
         System.out.println();
 
-        //Validação de Venda e HistoricoVendas
-        System.out.println("-----------------------------------------");
+        //Validação de Venda, ItemVendas e HistoricoVendas
+        System.out.println("-------------------Validação de Venda, ItemVendas e HistoricoVendas----------------------");
         Venda venda = new Venda(1);
         Venda venda2 = new Venda(2);
         Venda venda3 = new Venda(3);
         HistoricoVendas histVendas = new HistoricoVendas();
 
         //Adicionando itens a venda.
-        System.out.println("item inserido na venda 1: " + venda.insereItem(99, 2,estoque));
-        System.out.println("item inserido na venda 1: " + venda.insereItem(100, 5,estoque));
-        System.out.println("item removido na venda 1: " + venda.removeItem(2));
+        System.out.println("Item inserido na venda 1: " + venda.insereItem(99, 2,estoque));
+        System.out.println("Item inserido na venda 1: " + venda.insereItem(100, 5,estoque));
+        System.out.println("Item removido na venda 1: " + venda.removeItem(2));
         System.out.println();
 
         //Adicionando itens a venda2.
-        System.out.println("item inserido na venda 2: " + venda2.insereItem(99, 1,estoque));
+        System.out.println("Item inserido na venda 2: " + venda2.insereItem(99, 1,estoque));
         System.out.println();
 
         //Adicionando itens a venda3.
-        System.out.println("item inserido na venda 3: " + venda3.insereItem(99, 3,estoque));
+        System.out.println("Item inserido na venda 3: " + venda3.insereItem(99, 3,estoque));
         System.out.println();
 
         //ver com o professor a utilidade do metodo conclui.
